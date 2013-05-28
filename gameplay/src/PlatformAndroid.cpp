7ab08470ee7d4d845f4160aa50447c64f86d5873
Andrew Karpushin <reven86@gmail.com>
2013-05-28 23:23:49 +0400

    fixed resource path on android

--- gameplay/src/PlatformAndroid.cpp
+++ gameplay/src/PlatformAndroid.cpp
@@ -996,35 +996,6 @@ int Platform::enterMessagePump()
     }
     GP_ASSERT(env);
 
-    /* Get external files directory on Android; this will result in a directory where all app files
-     * should be stored, like /mnt/sdcard/android/<package-name>/files/
-     */
-    jboolean isCopy;
-
-    jclass clazz = env->GetObjectClass(activity->clazz);
-    jmethodID methodGetExternalStorage = env->GetMethodID(clazz, "getExternalFilesDir", "(Ljava/lang/String;)Ljava/io/File;");
-
-    jclass clazzFile = env->FindClass("java/io/File");
-    jmethodID methodGetPath = env->GetMethodID(clazzFile, "getPath", "()Ljava/lang/String;");
-
-    // Now has java.io.File object pointing to directory
-    jobject objectFile  = env->CallObjectMethod(activity->clazz, methodGetExternalStorage, NULL);
-    
-    // Now has String object containing path to directory
-    jstring stringExternalPath = static_cast<jstring>(env->CallObjectMethod(objectFile, methodGetPath));
-    const char* externalPath = env->GetStringUTFChars(stringExternalPath, &isCopy);
-
-    // Set the default path to store the resources.
-    std::string assetsPath(externalPath);
-    if (externalPath[strlen(externalPath)-1] != '/')
-        assetsPath += "/";
-
-    FileSystem::setResourcePath(assetsPath.c_str());    
-
-    // Release string data
-    env->ReleaseStringUTFChars(stringExternalPath, externalPath);
-    jvm->DetachCurrentThread();
-    
     // Get the asset manager to get the resources from the .apk file.
     __assetManager = activity->assetManager; 
     
