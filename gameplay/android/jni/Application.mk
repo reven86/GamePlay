3b66634df5235a448cbf4542418c589173e3c216
Andrew Karpushin <reven86@gmail.com>
2014-11-30 14:27:42 +0300

    shared stl libraries on Android

--- gameplay/android/jni/Application.mk
+++ gameplay/android/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_OPTIM := release
 NDK_TOOLCHAIN_VERSION := 4.8
 APP_CPPFLAGS += -std=c++11
-APP_STL      := gnustl_static
+APP_STL      := gnustl_shared
 APP_MODULES  := libgameplay
 APP_ABI      := armeabi-v7a x86
 APP_PLATFORM := android-16
