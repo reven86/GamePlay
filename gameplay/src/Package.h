ea086b0c15c73678659b60389c4a0cbf97c7b65c
Andrew Karpushin <reven86@gmail.com>
2015-10-12 02:23:09 +0300

    fixed indentation

--- gameplay/src/Package.h
+++ gameplay/src/Package.h
@@ -16,40 +16,40 @@ namespace gameplay
     {
     public:
         /**
-        * Destructor.
-        */
+         * Destructor.
+         */
         virtual ~Package();
 
         /**
-        * Opens a byte stream for the given resource path.
-        *
-        * If <code>path</code> is a file path, the file at the specified location is opened relative to the currently set
-        * resource path.
-        *
-        * @param path The path to the resource to be opened, relative to the currently set resource path.
-        * @param streamMode The stream mode used to open the file.
-        *
-        * @return A stream that can be used to read or write to the file depending on the mode.
-        *         Returns NULL if there was an error. (Request mode not supported).
-        *
-        * @script{ignore}
-        */
+         * Opens a byte stream for the given resource path.
+         *
+         * If <code>path</code> is a file path, the file at the specified location is opened relative to the currently set
+         * resource path.
+         *
+         * @param path The path to the resource to be opened, relative to the currently set resource path.
+         * @param streamMode The stream mode used to open the file.
+         *
+         * @return A stream that can be used to read or write to the file depending on the mode.
+         *         Returns NULL if there was an error. (Request mode not supported).
+         *
+         * @script{ignore}
+         */
         virtual Stream* open(const char* path, size_t streamMode = FileSystem::READ) = 0;
 
         /**
-        * Checks if the file at the given path exists.
-        *
-        * @param filePath The path to the file.
-        *
-        * @return <code>true</code> if the file exists; <code>false</code> otherwise.
-        */
+         * Checks if the file at the given path exists.
+         *
+         * @param filePath The path to the file.
+         *
+         * @return <code>true</code> if the file exists; <code>false</code> otherwise.
+         */
         virtual bool fileExists(const char* filePath) = 0;
 
     protected:
 
         /**
-        * Constructor.
-        */
+         * Constructor.
+         */
         Package();
     };
 
