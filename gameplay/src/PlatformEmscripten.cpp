976c3388d0a1a7b922bcb51cda3f39c05bb35244
Andrew Karpushin <reven86@gmail.com>
2016-04-13 19:26:11 +0300

    logging key events for emscripten

--- gameplay/src/PlatformEmscripten.cpp
+++ gameplay/src/PlatformEmscripten.cpp
@@ -879,6 +879,23 @@ EM_BOOL wheel_callback(int eventType, const EmscriptenWheelEvent *e, void *userD
     return 1;
 }
     
+EM_BOOL keyboard_callback(int eventType, const EmscriptenKeyboardEvent *e, void *userData)
+{
+    printf("%d key: %s code: %s ctrl: %d shift: %d alt: %d meta: %d charCode: %lu keyCode: %lu which: %lu\n", eventType,
+           e->key, e->code, e->ctrlKey, e->shiftKey, e->altKey, e->metaKey, e->charCode, e->keyCode, e->which);
+    
+    if (e->repeat)
+        return 1;
+    
+    /*
+    if (eventType == EMSCRIPTEN_EVENT_KEYDOWN)
+        gameplay::Platform::keyEventInternal(gameplay::Keyboard::KEY_PRESS, getKey(wParam, shiftDown ^ capsOn));
+    else if (eventType == EMSCRIPTEN_EVENT_KEYUP)
+        gameplay::Platform::keyEventInternal(gameplay::Keyboard::KEY_RELEASE, getKey(wParam, shiftDown ^ capsOn));
+    */
+    return 1;
+}
+    
 void main_loop_iter(void* _game)
 {
     Game* game = (Game*)_game;
@@ -930,6 +947,8 @@ int Platform::enterMessagePump()
     emscripten_set_mouseup_callback(0, 0, true, mouse_callback);
     emscripten_set_mousemove_callback(0, 0, true, mouse_callback);
     emscripten_set_wheel_callback("#canvas", 0, true, wheel_callback);
+    emscripten_set_keydown_callback(0, 0, true, keyboard_callback);
+    emscripten_set_keyup_callback(0, 0, true, keyboard_callback);
     emscripten_set_main_loop_arg(&main_loop_iter, (void *)_game, 0, 1);
 
     return 0;
@@ -997,14 +1016,14 @@ void Platform::setMultiSampling(bool enabled)
         return;
     }
 
-        // TODO
-        __multiSampling = enabled;
+    // TODO
+    __multiSampling = enabled;
 }
 
-    bool Platform::isMultiSampling()
-    {
-        return __multiSampling;
-    }
+bool Platform::isMultiSampling()
+{
+    return __multiSampling;
+}
 
 void Platform::setMultiTouch(bool enabled)
 {
