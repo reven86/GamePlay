dacaea54fcfcd304d77bcd8a023114ed3114afa5
Andrew Karpushin <reven86@gmail.com>
2022-09-05 16:27:57 +0300

    typo

--- gameplay/src/PlatformiOS.mm
+++ gameplay/src/PlatformiOS.mm
@@ -1892,21 +1892,21 @@ const char * Platform::getUserAgentString( )
 
 bool Platform::isTouchPressed()
 {
-    if (_tapRecognizer != NULL && _tapRecognizer.numberOfTouches > 0)
+    if (__view._tapRecognizer != NULL && __view._tapRecognizer.numberOfTouches > 0)
         return true;
-    if (_pinchRecognizer != NULL && _pinchRecognizer.numberOfTouches > 0)
+    if (__view._pinchRecognizer != NULL && __view._pinchRecognizer.numberOfTouches > 0)
         return true;
-    if (_rotationRecognizer != NULL && _rotationRecognizer.numberOfTouches > 0)
+    if (__view._rotationRecognizer != NULL && __view._rotationRecognizer.numberOfTouches > 0)
         return true;
-    if (_panRecognizer != NULL && _panRecognizer.numberOfTouches > 0)
+    if (__view._panRecognizer != NULL && __view._panRecognizer.numberOfTouches > 0)
         return true;
-    if (_swipeRecognizer != NULL && _swipeRecognizer.numberOfTouches > 0)
+    if (__view._swipeRecognizer != NULL && __view._swipeRecognizer.numberOfTouches > 0)
         return true;
-    if (_longPressRecognizer != NULL && _longPressRecognizer.numberOfTouches > 0)
+    if (__view._longPressRecognizer != NULL && __view._longPressRecognizer.numberOfTouches > 0)
         return true;
-    if (_longTapRecognizer != NULL && _longTapRecognizer.numberOfTouches > 0)
+    if (__view._longTapRecognizer != NULL && __view._longTapRecognizer.numberOfTouches > 0)
         return true;
-    if (_dragAndDropRecognizer != NULL && _dragAndDropRecognizer.numberOfTouches > 0)
+    if (__view._dragAndDropRecognizer != NULL && __view._dragAndDropRecognizer.numberOfTouches > 0)
         return true;
 
     int i = 0;
