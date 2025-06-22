382e7dd4f036f43fa4fa348cdd6197823cc705df
Andrew Karpushin <reven86@gmail.com>
2025-06-22 18:41:51 +0300

    use c++23 for ios and android

--- gameplay/android/jni/Application.mk
+++ gameplay/android/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_OPTIM := release
-APP_CPPFLAGS += -std=c++17 -fvisibility=hidden -fvisibility-inlines-hidden
+APP_CPPFLAGS += -std=c++23 -fvisibility=hidden -fvisibility-inlines-hidden
 APP_STL      := c++_static
 APP_MODULES  := libgameplay
 APP_ABI      := armeabi-v7a arm64-v8a x86 x86_64
