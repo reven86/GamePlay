4f51c82e547880616e55b8f37717b7a41fd4cf8e
Andrew Karpushin <reven86@gmail.com>
2025-10-11 17:56:41 +0300

    replace shader version string on mac

--- gameplay/src/Effect.cpp
+++ gameplay/src/Effect.cpp
@@ -272,7 +272,7 @@ Effect* Effect::createFromSource(const char* vshPath, const char* vshSource, con
 
 #if defined(__APPLE__) && TARGET_OS_MAC
     if (versionStr.empty())
-        versionStr = "#version 110\n";
+        versionStr = "#version 150 core\n";     // replace version string on mac
 #endif
 
     shaderSource[0] = versionStr.c_str();
@@ -335,8 +335,8 @@ Effect* Effect::createFromSource(const char* vshPath, const char* vshSource, con
     }
 
 #if defined(__APPLE__) && TARGET_OS_MAC
-    if (versionStr.empty())
-        versionStr = "#version 110\nprecision highp float;\n";
+    if (!versionStr.empty())
+        versionStr = "#version 150 core\n";     // replace version string on mac
 #endif
 
     shaderSource[0] = versionStr.c_str();
